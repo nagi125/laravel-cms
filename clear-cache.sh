e1590c140b71bbf1a56ea9dbf42c2974c02243ab
nagi125 <nagi125.dev@gmail.com>
2021-05-24 18:00:00 +0900

    cacheをクリアするシェルの追加

---
+++ clear-cache.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker-compose exec app sh -c "
+php artisan cache:clear &&
+php artisan config:clear &&
+php artisan config:cache &&
+php artisan route:clear &&
+php artisan view:clear &&
+php artisan clear-compiled &&
+composer dump-autoload &&
+rm -f bootstrap/cache/config.php"
