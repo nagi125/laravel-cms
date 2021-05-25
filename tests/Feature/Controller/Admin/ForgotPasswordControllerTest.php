f0d36e94bd28d0f93234bde3325a5a8b7eb67de8
nagi125 <nagi125.dev@gmail.com>
2021-05-25 16:59:24 +0900

    ForgotPasswordControllerのテスト追加

---
+++ tests/Feature/Controller/Admin/ForgotPasswordControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Controller\Admin;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class ForgotPasswordControllerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function パスワード再発行用メールアドレス入力画面は正常に表示される()
+    {
+        $res = $this->get(route('password.request'));
+        $res->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function パスワード再発行用メール送信処理は正常である()
+    {
+        $params = [
+            'email' => 'admin@example.com'
+        ];
+
+        $res = $this->post(route('password.email'), $params);
+        $res->assertStatus(302);
+    }
+
+}
