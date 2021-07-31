df42ba42c973047d4d9bb267aa5d1b5a638fe3d5
nagi125 <nagi125.dev@gmail.com>
2021-07-31 21:54:40 +0900

    必須バッジの追加/Submitの定義追加

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,28 +3,31 @@
 @section('description', $description ?? '')
 @section('content')
   <div>
-    <form>
-      <div class="form-group row">
-        <label for="name" class="col-sm-2 col-form-label">お名前</label>
-        <div class="col-sm-10">
-          <input type="text" class="form-control" id="name" placeholder="苗字 名前">
-        </div>
+    {{ Form::open(['route' => ['contact.submit'], 'method' => 'post']) }}
+    @csrf
+    @method('post')
+    <div class="form-group row">
+      <label for="name" class="text-left col-sm-3 col-form-label"><span class="badge-danger p-2 mr-1">必須</span>お名前</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" id="name" placeholder="苗字 名前">
       </div>
-      <div class="form-group row">
-        <label for="email" class="col-sm-2 col-form-label">メール</label>
-        <div class="col-sm-10">
-          <input type="email" class="form-control" id="email" value="email@example.com">
-        </div>
+    </div>
+    <div class="form-group row">
+      <label for="email" class="text-left col-sm-3 col-form-label"><span class="badge-danger p-2 mr-1">必須</span>メール</label>
+      <div class="col-sm-9">
+        <input type="email" class="form-control" id="email" value="email@example.com">
       </div>
-      <div class="form-group row">
-        <label for="content" class="col-sm-2 col-form-label">内容</label>
-        <div class="col-sm-10">
-          <textarea class="form-control" name="content" id="content" cols="30" rows="5" placeholder="お問い合わせ内容"></textarea>
-        </div>
+    </div>
+    <div class="form-group row">
+      <label for="body" class="text-left col-sm-3 col-form-label"><span class="badge-danger p-2 mr-1">必須</span>内容</label>
+      <div class="col-sm-9">
+        <textarea class="form-control" name="body" id="body" cols="30" rows="5" placeholder="お問い合わせ内容"></textarea>
       </div>
-      <div class="text-right">
-        <button class="btn btn-secondary px-4">送信</button>
-      </div>
+    </div>
+    <div class="text-right">
+      {{ Form::submit('送信', ['class' => 'btn btn-secondary px-3']) }}
+    </div>
     </form>
+    {{ Form::close() }}
   </div>
 @endsection
