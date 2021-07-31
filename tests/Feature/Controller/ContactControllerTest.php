73904b3bf435bc0b91bca175079c1af58ae6b8b9
nagi125 <nagi125.dev@gmail.com>
2021-07-31 23:25:09 +0900

    お問い合わせページのテスト追加

--- tests/Feature/Controller/ContactControllerTest.php
+++ tests/Feature/Controller/ContactControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Controller;
 
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class ContactControllerTest extends TestCase
@@ -11,7 +13,36 @@ class ContactControllerTest extends TestCase
      */
     public function お問い合わせページは正常に表示される()
     {
-        $response = $this->get('/contact');
-        $response->assertStatus(200);
+        $res = $this->get(route('contact.index'));
+        $res->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function お問い合わせのサンキューページは正常に表示される()
+    {
+        $res = $this->get(route('contact.thanks'));
+        $res->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function お問い合わせ処理は正常に動作する()
+    {
+        $postData = [
+          'name'  => '野末 テスト',
+          'email' => 'nagi125.dev@gmail.com',
+          'body'  => 'test',
+        ];
+
+        Mail::fake();
+
+        $res = $this->from(route('contact.index'))->post(route('contact.submit'), $postData);
+
+        Mail::assertSent(ContactMail::class, 1);
+
+        $res->assertRedirect(route('contact.thanks'));
     }
 }
