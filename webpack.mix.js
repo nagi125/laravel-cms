2480204ea3c3b81d6cb640b2d4b91ff08b02bf29
nagi125 <nagi125.dev@gmail.com>
2021-11-04 20:45:48 +0900

    fix: フロントエンドのBuildコマンド周り修正

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ mix
     .sass('resources/sass/app.scss', 'public/css/app.css')
     .sass('resources/sass/admin/app.scss', 'public/css/app_admin.css')
     .js('resources/js/app.js', 'public/js/app.js')
-    .js('resources/js/admin/app.js', 'public/js/app_admin.js');
+    .js('resources/js/admin/app.js', 'public/js/app_admin.js')
+    .vue();
