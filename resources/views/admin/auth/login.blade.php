2b2e0cb36f6bea7d1867956b512eaef355581a49
nagi125 <nagi125.dev@gmail.com>
2021-11-04 22:07:33 +0900

    ログイン画面のデザイン修正

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -10,8 +10,8 @@
           <div class="card-body">
             <form method="POST" action="{{ route('login') }}">
               @csrf
-              <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+              <div class="form-group row mb-3">
+                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
                 <div class="col-md-6">
                   <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                   @error('email')
@@ -22,8 +22,8 @@
                 </div>
               </div>
 
-              <div class="form-group row">
-                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+              <div class="form-group row mb-3">
+                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
                 <div class="col-md-6">
                   <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                   @error('password')
@@ -34,7 +34,7 @@
                 </div>
               </div>
 
-              <div class="form-group row">
+              <div class="form-group row mb-3">
                 <div class="col-md-6 offset-md-4">
                   <div class="form-check">
                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
