55b0ca564c51336f6e45e7c2e842445ed0354d15
nagi125 <nagi125.dev@gmail.com>
2021-11-08 16:59:47 +0900

    fix: ユーザー作成・編集ページを表示できるように修正

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('admin._layouts.layout')
+@extends('admin._layouts.app')
 @section('title', $title)
 @section('content')
   <h4 class="c-grey-900 mT-10 mB-30">{{ $title }}</h4>
@@ -12,8 +12,8 @@
     <form name="delete" method="POST" action="{{ route('admin.users.destroy', ['user' => $user]) }}" class="d-inline">
       @csrf
       @method('DELETE')
-      <input type="button" class="btn btn-outline-danger" role="button" data-toggle="modal" data-target="#deleteModal" value="削除">
-      @include('admin._components.confirm_delete_modal')
+      <input type="button" class="btn btn-outline-danger" role="button" data-bs-toggle="modal" data-bs-target="#deleteModal" value="削除">
+      @include('admin._partials.confirm_delete_modal')
     </form>
   </div>
 @endsection
